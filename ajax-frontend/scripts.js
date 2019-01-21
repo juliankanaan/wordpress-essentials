03feae6135343f5e8a4b38cdd1a58e0be05bfce4
juliankanaan <46693750+juliankanaan@users.noreply.github.com>
2019-01-21 17:18:50 -0600

    Update scripts.js

--- ajax-frontend/scripts.js
+++ ajax-frontend/scripts.js
@@ -1,1 +1,43 @@
 
+jQuery(document).ready(function() {
+
+  jQuery("#trigger").click(function(e) {
+    // prevent from happening on page load
+    e.preventDefault();
+    // going to pass a random number
+    num = jQuery.randomBetween(0, 10);
+    // change color of button on click
+
+    jQuery("#trigger").css("background-color", "#ccc");
+
+    // ajax post starts here
+
+    jQuery.ajax({
+         type : "post",
+         dataType : "json",
+         url : myAjax.ajaxurl,
+         data : {action: "random", num: num},
+         success: function(response) {
+           // check for success & do something:
+            if(response.type == "success") {
+
+              alert("Success");
+
+              number = response.new_num;
+              // alert user to what the server said
+              alert(number);
+
+            }
+            else {
+               alert("Something failed with the AJAX request");
+            }
+         }
+      });
+
+
+  });
+
+
+
+});
+
